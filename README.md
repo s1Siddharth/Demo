0219bf298f9c7a0015b63a257f43999ce8204458
s1Siddharth <1laptopcode@gmail.com>
2025-03-14 12:35:31 +0530

    python code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Demo
 This is my first git repo.
 <br>
-Author-Siddharth
+Author-Siddharth(hacker bolte)
---
+++ main.py
@@ -0,0 +1,4 @@
+n=int(input("Enter a number :"))
+print(f"The table of {n}")
+for num in range(1,11):
+    print(f"{n} x {num} = {n*num}")
